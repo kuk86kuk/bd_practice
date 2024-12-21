6afcd5d6ce1aed50932d0357a5d4dad79b8910fe
Treglazov Egor <etreglazov@datanomica.org>
2024-12-21 15:58:36 +0300

    add lab06

---
+++ lab06/spark.py
@@ -0,0 +1,5 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col #<put your code here>
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+#<put your code here>
